73f91a48cbb8323ff8d10351a32eeb81ae531a17
deesin <deesin@on.sinch.com>
2022-03-13 16:07:47 +0100

    initial commit

---
+++ src/main/java/com/practice/kafka/tutorial1/ProducerDemo.java
@@ -0,0 +1,33 @@
+package com.practice.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        String bootstrapServers = "127.0.0.1:9092";
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //Create Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        //Create producerRecord
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "Hello World");
+
+        // send Data
+        producer.send(record);
+
+        //flush and close
+        producer.flush();
+        producer.close();
+
+    }
+}
